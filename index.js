c599a3abd0b02d523c6282d3a8ae1557e6baa866
Moshiur-15 <masiurislam28@gmail.com>
2024-12-25 19:46:13 +0600

    wishlist complate

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000
 
 
 app.use(cors({
-  origin: ['http://localhost:5174'],
-  credentials:true
+  origin: ['http://localhost:5173', 'http://localhost:3001'],
+  credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
@@ -52,7 +52,7 @@ async function run() {
     const blogsCollection = client.db('Blogs-collection').collection('Blogs')
     const wishlistCollection = client.db('Blogs-collection').collection('wishlist')
     const commentCollection = client.db('Blogs-collection').collection('comment')
-    
+    // Jwt Token
     // create jwt token
     app.post('/jwt', async(req, res)=>{
       const user = req.body;
@@ -66,9 +66,12 @@ async function run() {
     })
     // sign out and token delete
     app.post('/signOut', (req, res) => {
-      res.clearCookie('token', { httpOnly: true, secure: false }).send({success:true});
+      res
+      .clearCookie('token', { httpOnly: true, secure: false })
+      .send({success:true});
     })
 
+    // blogsCollection 
     // blogs server get request
     app.get('/blogs', async (req, res) => {
       const filter = req.query.filter;
@@ -83,40 +86,12 @@ async function run() {
       const result = await blogsCollection.find(query).toArray();
       res.send(result)
     })
-
-    // wishlist server get request
-    app.get('/wishlist', async (req, res) => {
-      const result = await wishlistCollection.find().toArray();
-      res.send(result)
-    })
-
-    // comment server get request
-    app.get('/comment', async (req, res) => {
-      const result = await commentCollection.find().toArray();
-      res.send(result)
-    })
-
     // blogs save post request
     app.post('/add-blogs', async (req, res) => {
       const blog = req.body;
       const result = await blogsCollection.insertOne(blog)
       res.send(result)
     })
-
-    // wishlist save post request
-    app.post('/add-wishlist', async (req, res) => {
-      const wishlist = req.body;
-      const result = await wishlistCollection.insertOne(wishlist)
-      res.send(result)
-    })
-
-    // comment save post request
-    app.post('/add-comment', async (req, res) => {
-      const comment = req.body;
-      const result = await commentCollection.insertOne(comment)
-      res.send(result)
-    })
-    
     // blogs details post request
     app.get('/unique-blog/:id', async (req, res) => {
       const id = req.params.id
@@ -124,15 +99,6 @@ async function run() {
       const result = await blogsCollection.findOne(query)
       res.send(result)
     })
-
-    // comment get request
-    app.get('/comments/:id', async (req, res) => {
-      const blogId  = req.params.id
-      const query = {blog_id: blogId }
-      const result = await commentCollection.find(query).toArray();
-      res.send(result)
-    })
-
     // update blogs
     app.put('/unique-blog/:id', async (req, res) => {
       const id = req.params.id;
@@ -143,6 +109,55 @@ async function run() {
         $set: updateBlog,
       }
       const result = await blogsCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+    // wishlistCollection
+    // wishlist server get request
+    app.get('/wishlist', async (req, res) => {
+      const result = await wishlistCollection.find().toArray();
+      res.send(result)
+    })
+    // wishlist save post request
+    app.post('/add-wishlist', async (req, res) => {
+      const wishlist = req.body;
+      const result = await wishlistCollection.insertOne(wishlist)
+      res.send(result)
+    })
+
+    // user wishlist data
+    app.get('/wishlist/:email', async (req, res) => {
+      const email = req.params.email
+      const query ={email: email}
+      const result = await wishlistCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/delete/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id:new ObjectId(id) }
+      const result = await wishlistCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+    // commentCollection  
+    // comment server get request
+    app.get('/comment', async (req, res) => {
+      const result = await commentCollection.find().toArray();
+      res.send(result)
+    })
+    // comment save post request
+    app.post('/add-comment', async (req, res) => {
+      const comment = req.body;
+      const result = await commentCollection.insertOne(comment)
+      res.send(result)
+    })
+    // comment get request
+    app.get('/comments/:id', async (req, res) => {
+      const blogId  = req.params.id
+      const query = {blog_id: blogId }
+      const result = await commentCollection.find(query).toArray();
       res.send(result)
     })
     
